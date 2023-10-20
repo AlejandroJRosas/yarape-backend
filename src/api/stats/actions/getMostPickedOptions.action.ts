d596acd65d7b5cd4ec35a210a066c604080a0be8
Alejandro <alejandrojrosas2001@gmail.com>
2023-10-19 20:01:33 -0400

    fixed most-picked-options action

--- src/api/stats/actions/getMostPickedOptions.action.ts
+++ src/api/stats/actions/getMostPickedOptions.action.ts
@@ -4,36 +4,51 @@ import { STATUS } from '../../../utils/constants'
 import { handleControllerError } from '../../../utils/responses/handleControllerError'
 
 export const getMostPickedOptions = async (
-  _: Request,
+  req: Request,
   res: Response
 ): Promise<Response> => {
   try {
-    const { rows: avg } = await pool.query({
-      text: `
-        SELECT
-          qo.category_id,
-          qo.question_id,
-          qo.option_id,
-          qo.description AS x,
-          COALESCE(COUNT(a.option_id), 0) AS y
-        FROM
-          quest_options AS qo
-          JOIN questions AS q ON qo.category_id = q.category_id AND qo.question_id = q.question_id
-          LEFT JOIN answers AS a ON q.category_id = a.category_id AND q.question_id = a.question_id AND qo.option_id = a.option_id
-        GROUP BY
-          qo.category_id,
-          qo.question_id,
-          qo.option_id,
-          qo.description
-        ORDER BY
-          qo.category_id,
-          qo.question_id,
-          qo.option_id;
-      `
+    // const discriminantOnlyOption = 'u.is_ucab_member = true AND'
+    // const discriminantCampusOption = 'u.campus_id = 1 AND'
+    // const discriminantRoleOption = 'u.role IN (\'T\')'
+
+    const text = `
+      SELECT
+        q.description AS description,
+        qo.description AS x,
+        COUNT(a.option_id) AS y
+      FROM
+        questions AS q
+        JOIN quest_options AS qo
+          ON q.category_id = qo.category_id
+          AND q.question_id = qo.question_id
+        LEFT JOIN answers AS a
+          ON qo.category_id = a.category_id
+          AND qo.question_id = a.question_id
+          AND qo.option_id = a.option_id,
+        users AS u
+      WHERE
+        a.user_id = u.user_id AND
+        q.category_id = $1 AND
+        q.question_id = $2
+      GROUP BY
+        q.description,
+        qo.description,
+        q.category_id,
+        q.question_id;
+    `
+
+    if (req.query?.onlyUcab === 'true') {
+      console.log('hola')
+    }
+
+    const { rows: most } = await pool.query({
+      text,
+      values: [req.params.categoryId, req.params.questionId]
     })
 
     return res.status(STATUS.OK).json({
-      items: [...avg]
+      items: [...most]
     })
   } catch (error: unknown) {
     return handleControllerError(error, res)
